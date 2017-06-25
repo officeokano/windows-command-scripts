df020c682fc67a675309ec7c42081e4868d92996
okano <okano@bbcoms.co.jp>
2017-06-25 11:08:14 +0900

    edit message of chkadmin.cmd

--- chkadmin.cmd
+++ chkadmin.cmd
@@ -3,13 +3,13 @@
     for /f "tokens=1 delims=," %%i in ('whoami /groups /FO CSV /NH') do (
         if "%%~i"=="BUILTIN\Administrators" goto admin
     )
-    echo This command must run as an administrator.
+    echo This command must be run as an administrator.
     exit /b 1
 :admin
     for /f "tokens=1 delims=," %%i in ('whoami /groups /FO CSV /NH') do (
         if "%%~i"=="Mandatory Label\High Mandatory Level" goto main
     )
-    echo This command must run as an administrator.
+    echo This command must be run as an administrator.
     exit /b 1
 :main
     exit /b 0
